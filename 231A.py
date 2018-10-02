259fcc0d81ac3ecbe764a6b893e1f4c6c16f93de
Guvvala-Prasanth-Reddy <36601101+Guvvala-Prasanth-Reddy@users.noreply.github.com>
2018-10-02 22:20:55 +0530

    added sum

--- 231A.py
+++ 231A.py
@@ -2,7 +2,7 @@ n = int(input())
 count = 0
 for i in range(n):
     a = list(map(int, input().strip().split( " ")))        
-    if a[0]+a[1]+a[2] >= 2:
+    if sum(a) >= 2:
         count = count + 1
 print (count)
         
